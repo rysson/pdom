5a47dc8197da76cfe7b73626c2b29be1d9e3dd6f
Robert Kalinowski <robert.kalinowski@sharkbits.com>
2018-06-26 13:03:34 +0200

    Init parseDOM (mrknow, cherry)

---
+++ ParseDOM/cherry/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import re
+from . import dom_parser
+
+def parseDOM(html, name='', attrs=None, ret=False):
+    if attrs: attrs = dict((key, re.compile(value + ('$' if value else ''))) for key, value in attrs.iteritems())
+    results = dom_parser.parse_dom(html, name, attrs, ret)
+    if ret:
+        results = [result.attrs[ret.lower()] for result in results]
+    else:
+        results = [result.content for result in results]
+    return results
+
